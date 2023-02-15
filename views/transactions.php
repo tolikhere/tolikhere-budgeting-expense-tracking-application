11b4b5b7ca902c45fb8375eb6bbfbaa4bcd073e0
Anatoliy Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-02-15 06:14:39 +0300

    Initial files

---
+++ views/transactions.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Transactions</title>
+        <link rel="stylesheet" href="./css/styles.css">
+    </head>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Check #</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- YOUR CODE -->
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="3">Total Income:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+                <tr>
+                    <th colspan="3">Total Expense:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+                <tr>
+                    <th colspan="3">Net Total:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+            </tfoot>
+        </table>
+    </body>
+</html>
